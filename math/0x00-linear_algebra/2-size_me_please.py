2324451fc46e2bf4d88880f8a9c50b1f1b189733
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-16 05:21:12 +0000

    2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-"""
-wrapper to keep state of shape
-"""
-
-
 def _matrix_shape(matrix):
-
+    """
+    wrapper for keep shape state
+    """
     global shape
     for i in matrix:
         if type(i) is list:
             shape.append(len(i))
             _matrix_shape(i)
         return shape
-
-
-"""
-the main function
-"""
 
 
 def matrix_shape(matrix):
