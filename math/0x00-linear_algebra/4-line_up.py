b7bb354769aaf80671984e83ed486ffc4e240362
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-16 05:52:46 +0000

    4

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+wrapper
+"""
+
+
+def _matrix_shape(matrix):
+    """
+    wrapper for keep shape state
+    """
+    global shape
+    for i in matrix:
+        if type(i) is list:
+            shape.append(len(i))
+            _matrix_shape(i)
+        return shape
+
+
+"""
+main
+"""
+
+
+def matrix_shape(matrix):
+    """
+    main
+    """
+    global shape
+    shape = [len(matrix)]
+    return _matrix_shape(matrix)
+
+
+"""
+adds two array element-wise
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    main
+    """
+    if matrix_shape(arr1) != matrix_shape(arr2):
+        return None
+    return [(i + j) for i, j in zip(arr1, arr2)]
