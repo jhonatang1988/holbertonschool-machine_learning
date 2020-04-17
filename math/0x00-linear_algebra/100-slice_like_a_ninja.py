e74eb95edb62fb34f0608ef47b05ced49a160392
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-17 00:35:12 +0000

    100

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+slic matrix with specific axis
+"""
+
+
+def np_slice(matrix, axes={}):
+    """
+    main
+    """
+    alist = []
+    for i in range(len(matrix.shape)):
+        if i in axes:
+            alist.append(slice(*axes[i]))
+        else:
+            alist.append(None)
+    atuple = tuple(alist)
+    return matrix[atuple]
