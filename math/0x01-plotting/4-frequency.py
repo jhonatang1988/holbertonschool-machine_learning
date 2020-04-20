33f704a789c5128b92c74cdcdd2b4e66a8405037
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-20 17:01:14 -0500

    4

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+fig, ax = plt.subplots()
+ax.hist(student_grades, bins=10, range=(0, 100),
+        edgecolor='black')
+plt.xticks(range(0, 101, 10))
+plt.xlim([0, 100])
+plt.ylim([0, 30])
+plt.xlabel("Grades")
+plt.ylabel('Number of Students')
+plt.title('Project A')
