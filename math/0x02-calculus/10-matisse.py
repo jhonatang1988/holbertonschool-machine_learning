de767345530f749ff9bcb4f8c6d2ad2e522119d8
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-26 16:08:34 -0500

    10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,6 +5,11 @@ derivatives for polys
 
 
 def poly_derivative(poly):
+    """
+    main
+    :param poly: polynomial
+    :return: derivative of polynomial
+    """
     derivates = []
     for i in range(len(poly) - 1, 0, -1):
         derivates.append(poly[i] * i)
