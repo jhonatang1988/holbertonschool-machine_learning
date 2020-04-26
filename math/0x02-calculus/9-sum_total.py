9bf48807ae2e0e6fe5a366da2cc6624b58efb741
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-26 13:46:08 -0500

    9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
+"""
+sigma notation
+"""
+
+
 def _summation_i_squared(n):
+    """
+    recursive helper
+    :param n: limit to sum
+    :return: nothing
+    """
     global counter
     global result
-    result = result + n**2
+    result = result + n ** 2
     if counter == limit:
         return
     counter = counter + 1
     _summation_i_squared(counter)
 
+
 def summation_i_squared(n):
+    """
+    main
+    :param n: limit to sum
+    :return: sum
+    """
     global limit
     global counter
     global result
