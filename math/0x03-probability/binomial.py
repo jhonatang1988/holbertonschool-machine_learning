00778e1ba9a68a5e9809367142d194db98d4e3f9
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-04-30 22:26:27 -0500

    10

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -4,15 +4,47 @@ represents a binomial distribution
 """
 
 
+def toFloat(lambtha):
+    """
+    :param lambtha: to convert to float
+    :return: float or exception
+    """
+    try:
+        return float(lambtha)
+    except TypeError:
+        print('it must be convertible to float')
+
+
+def toint(k):
+    """
+    :param k: param to convert to int
+    :return: int or exception
+    """
+    try:
+        k = int(k)
+        return k
+    except TypeError:
+        print('value must be an integer or float')
+
+
 class Binomial:
+    """
+    class Binomial
+    """
+
     def __init__(self, data=None, n=1, p=0.5):
+        """
+        :param data: list of the data to be used to estimate the distribution
+        :param n: number of Bernoulli trials
+        :param p: probability of a “success”
+        """
         if n <= 0:
             raise ValueError('n must be a positive value')
         elif 0 >= p <= 1:
             raise ValueError('p must be greater than 0 and less than 1')
         elif data is None:
-            self.n = n
-            self.p = p
+            self.n = toint(n)
+            self.p = toint(p)
         elif type(data) is not list:
             raise TypeError('data must be a list')
         elif len(data) < 2:
