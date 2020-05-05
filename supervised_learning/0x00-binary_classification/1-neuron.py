81ac33df89fc9f58d6522d8efae257bad1c35ce2
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-05 13:03:17 -0500

    1

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Neuron Class
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Neuron for binary classification
+    """
+
+    def __init__(self, nx):
+        """
+        __init__
+        :param nx: number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        else:
+            self.W = np.random.randn(1, nx)
+            self._b = 0
+            self._A = 0
+
+    @property
+    def b(self):
+        """
+        biases getter
+        :return: private biases
+        """
+        return self._b
+
+    @property
+    def A(self):
+        """
+        A for Activation getter
+        :return: private activation
+        """
+        return self._A
