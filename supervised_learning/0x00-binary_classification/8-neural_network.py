0aa8acd2d385f87e51a2310acb0a5f9edffa9af7
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-06 14:43:56 -0500

    8

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -2,10 +2,20 @@
 """
 Neural network for binary classification
 """
+import numpy as np
 
 
 class NeuralNetwork:
+    """
+    NeuralNetwork class
+    """
+
     def __init__(self, nx, nodes):
+        """
+        init NeuralNetwork instance
+        :param nx: number of inputs
+        :param nodes: number of layers
+        """
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         elif nx < 1:
@@ -14,3 +24,10 @@ class NeuralNetwork:
             raise TypeError('nodes must be an integer')
         elif nodes < 1:
             raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
