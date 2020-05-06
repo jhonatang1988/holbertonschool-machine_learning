29e5c4dae299447162aad8f7a72a1246426d9538
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-06 14:56:55 -0500

    9

---
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Neural network for binary classification
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    NeuralNetwork class
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        init NeuralNetwork instance
+        :param nx: number of inputs
+        :param nodes: number of layers
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        elif type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        elif nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
