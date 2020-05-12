0eadf9e91c5a3b720125d52f404e725f0561cb9d
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-12 15:17:06 -0500

    1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -13,33 +13,20 @@ def one_hot_decode(one_hot):
     """
     if type(one_hot) is not np.ndarray:
         return None
-    elif one_hot.shape[1] == 0:
+    elif len(one_hot) == 0:
         return None
-    # elif len(one_hot.shape) != 2:
+    elif len(one_hot.shape) != 2:
+        return None
+    elif np.amax(one_hot) > 1.0:
+        return None
+    elif np.sum(one_hot) > one_hot.shape[1]:
+        return None
+    for class_vector in one_hot.T:
+        # print(np.sum(class_vector))
+        if np.sum(class_vector) != 1.0:
+            return None
+    # if type(one_hot) is not np.ndarray or len(one_hot) == 0:
     #     return None
-    # elif np.amax(one_hot) > 1.0:
-    #     return None
-    # elif np.sum(one_hot) > one_hot.shape[1]:
-    #     return None
-    # for class_vector in one_hot.T:
-    #     # print(np.sum(class_vector))
-    #     if np.sum(class_vector) != 1.0:
-    #         return None
-
-    # has to be shape 2 => (classes, m)
-    if len(one_hot.shape) != 2:
-        return None
-
-        # only can contain 1 and  0
-    b = np.where((one_hot != 0) & (one_hot != 1), True, False)
-    if b.any() is True:
-        return None
-
-    # only can has one 1 per column
-    b = one_hot.T.sum(axis=1)
-    b = np.where(b > 1, True, False)
-    if b.any() is True:
-        return None
 
     a_list = []
     for i in range(one_hot.shape[1]):
