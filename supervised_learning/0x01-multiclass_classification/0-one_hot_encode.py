1dcc789cb89c6a5a9b46049a881831563d1996b9
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-12 12:06:42 -0500

    1

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -12,6 +12,14 @@ def one_hot_encode(Y, classes):
     :param classes: max number of classes
     :return: a one-hot encoding of Y shape (classes, m) or NONE if failure
     """
+    if type(Y) is not np.ndarray:
+        return None
+    elif len(Y) == 0:
+        return None
+    elif type(classes) is not int:
+        return None
+    elif classes <= np.amax(Y, axis=0):
+        return None
     a_list = []
     for i in range(classes):
         array = np.zeros(classes)
@@ -19,5 +27,5 @@ def one_hot_encode(Y, classes):
         index = Y[i]
         array[index] = 1
         a_list.append(array)
-    one_hot_encode = np.array(a_list).T
+    one_hot_encode = np.asarray(a_list).T
     return one_hot_encode
