851fea8072f67308b8319a011cecb8bba165c1a4
Jhonatan Galindo <950@holbertonschool.com>
2020-05-18 23:52:25 +0000

    6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -26,49 +26,38 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
     iterations: param: is the number of iterations to train over
     save_path: param: designates where to save the model
     """
-    # print(X_train.shape[1])
-    # print(Y_train.shape)
-    # print(X_valid.shape)
-    # print(Y_valid.shape)
-    # print(layer_sizes)
-    # print(activations)
+    print(X_train.shape[1])
+    print(Y_train.shape)
+    print(X_valid.shape)
+    print(Y_valid.shape)
+    print(layer_sizes)
+    print(activations)
 
-    # x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    # # print(x)
-    # # print(y)
-    # tf.add_to_collection('x', x)
-    # tf.add_to_collection('y', x)
-
-    # y_pred = forward_prop(x, layer_sizes, activations)
-    # # print(y_pred)
-    # tf.add_to_collection('y_pred', y_pred)
-
-    # accuracy = calculate_accuracy(y, y_pred)
-    # # print(accuracy)
-    # tf.add_to_collection('accuracy', accuracy)
-
-    # loss = calculate_loss(y, y_pred)
-    # # print(loss)
-    # tf.add_to_collection('loss', loss)
-
-    # train_op = create_train_op(loss, alpha)
-    # # print(train_op)
-    # tf.add_to_collection('train_op', train_op)
-
-    # init_op = tf.global_variables_initializer()
-    # # print(init_op)
-    # saver = tf.train.Saver()
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    # print(x)
+    # print(y)
     tf.add_to_collection('x', x)
-    tf.add_to_collection('y', y)
+    tf.add_to_collection('y', x)
+
     y_pred = forward_prop(x, layer_sizes, activations)
+    # print(y_pred)
     tf.add_to_collection('y_pred', y_pred)
-    loss = calculate_loss(y, y_pred)
-    tf.add_to_collection('loss', loss)
+
     accuracy = calculate_accuracy(y, y_pred)
+    # print(accuracy)
     tf.add_to_collection('accuracy', accuracy)
+
+    loss = calculate_loss(y, y_pred)
+    # print(loss)
+    tf.add_to_collection('loss', loss)
+
     train_op = create_train_op(loss, alpha)
+    # print(train_op)
     tf.add_to_collection('train_op', train_op)
+
+    init_op = tf.global_variables_initializer()
+    # print(init_op)
+    saver = tf.train.Saver()
 
     init_op = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -95,4 +84,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
             sess.run(train_op, feed_dict={
                      x: X_train, y: Y_train}) if i < iterations else None
 
-    return saver.save(sess, save_path)
+    saved_model = saver.save(sess, save_path)
+
+    return saved_model
