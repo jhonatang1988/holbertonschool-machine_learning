c1dbd6c5b869f7e54b839eda05e956982dbc7286
Jhonatan Galindo <950@holbertonschool.com>
2020-05-18 21:32:02 +0000

    6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -65,14 +65,15 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
             acc_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             cost_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
             acc_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-            print('After {} iterations:'.format(i)) if i % 100 == 0 else None
+            print('After {} iterations:'.format(i)
+                  ) if i % 100 == 0 or i == iterations else None
             print('\tTraining Cost: {}'.format(
-                cost_train)) if i % 100 == 0 else None
+                cost_train)) if i % 100 == 0 or i == iterations else None
             print('\tTraining Accuracy: {}'.format(
-                acc_train)) if i % 100 == 0 else None
+                acc_train)) if i % 100 == 0 or i == iterations else None
             print('\tValidation Cost: {}'.format(
-                cost_valid))if i % 100 == 0 else None
+                cost_valid)) if i % 100 == 0 or i == iterations else None
             print('\tValidation Accuracy: {}'.format(
-                acc_valid)) if i % 100 == 0 else None
+                acc_valid))if i % 100 == 0 or i == iterations else None
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         return saver.save(sess, save_path)
