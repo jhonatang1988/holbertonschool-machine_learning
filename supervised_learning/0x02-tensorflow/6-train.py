0d7a02268cf97f235ebd706d1d1fa4439809cb17
Jhonatan Galindo <950@holbertonschool.com>
2020-05-19 00:46:15 +0000

    6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -70,16 +70,12 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
             acc_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             cost_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
             acc_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-            print('After {} iterations:'.format(i)
-                  ) if i % 100 == 0 or i == iterations else None
-            print('\tTraining Cost: {}'.format(
-                cost_train)) if i % 100 == 0 or i == iterations else None
-            print('\tTraining Accuracy: {}'.format(
-                acc_train)) if i % 100 == 0 or i == iterations else None
-            print('\tValidation Cost: {}'.format(
-                cost_valid)) if i % 100 == 0 or i == iterations else None
-            print('\tValidation Accuracy: {}'.format(
-                acc_valid)) if i % 100 == 0 or i == iterations else None
+            if i % 100 == 0 or i == iterations:
+                print('After {} iterations:'.format(i))
+                print('\tTraining Cost: {}'.format(cost_train))
+                print('\tTraining Accuracy: {}'.format(acc_train))
+                print('\tValidation Cost: {}'.format(cost_valid))
+                print('\tValidation Accuracy: {}'.format(acc_valid))
 
             sess.run(train_op, feed_dict={
                      x: X_train, y: Y_train}) if i < iterations else None
