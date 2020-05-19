712f1bdf063943a9e56a58331a350d61e4d72dd6
Jhonatan Galindo <950@holbertonschool.com>
2020-05-19 00:47:58 +0000

    6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -64,21 +64,21 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
 
     with tf.Session() as sess:
         sess.run(init_op)
-        for i in range(0, iterations + 1, 1):
-            cost_train = sess.run(
-                loss, feed_dict={x: X_train, y: Y_train})
+        for i in range(iterations + 1):
+            cost_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             acc_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             cost_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
             acc_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-            if i % 100 == 0 or i == iterations:
-                print('After {} iterations:'.format(i))
-                print('\tTraining Cost: {}'.format(cost_train))
-                print('\tTraining Accuracy: {}'.format(acc_train))
-                print('\tValidation Cost: {}'.format(cost_valid))
-                print('\tValidation Accuracy: {}'.format(acc_valid))
 
-            sess.run(train_op, feed_dict={
-                     x: X_train, y: Y_train}) if i < iterations else None
+            if i % 100 == 0 or i == iterations:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(cost_train))
+                print("\tTraining Accuracy: {}".format(acc_train))
+                print("\tValidation Cost: {}".format(cost_valid))
+                print("\tValidation Accuracy: {}".format(acc_valid))
+
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
         saved_model = saver.save(sess, save_path)
 
