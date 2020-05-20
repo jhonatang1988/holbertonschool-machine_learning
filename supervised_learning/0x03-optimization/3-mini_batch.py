5b527057475a3517a232c6a4078e324455c4037e
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-20 16:21:10 -0500

    3

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -2,10 +2,29 @@
 """
 that trains a loaded neural network model using mini-batch gradient descent:
 """
-import numpy as np
 import tensorflow as tf
 
 shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+"""
+returns a mini batch
+"""
+
+
+def iterate_mini_batches(X, Y, batch_size):
+    """
+    to get the mini_batch
+    :param X: input data
+    :param Y: labels
+    :param batch_size: size of mini batch
+    :return: mini_batch
+    """
+    j = 0
+    for i in range(0, X.shape[0] - batch_size + 1, batch_size):
+        j = j + 1
+        X_shuffled, Y_shuffled = shuffle_data(X, Y)
+        excerpt = slice(i, i + batch_size)
+        yield X_shuffled[excerpt], Y_shuffled[excerpt], j
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
@@ -24,9 +43,8 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     :param save_path: path to where the model should be saved after training
     :return: the path where the model was saved
     """
-    saver = tf.train.Saver()
     with tf.Session() as sess:
-        loader = tf.import_meta_graph(load_path + 'meta')
+        loader = tf.train.import_meta_graph(load_path + '.meta')
         loader.restore(sess, load_path)
 
         x = tf.get_collection('x')[0]
@@ -53,38 +71,20 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
             print('\tValidation Cost: {}'.format(valid_cost))
             print('\tValidation Accuracy: {}'.format(valid_accuracy))
 
-            for i in range(X_train.shape[0] / batch_size):
-                X_batch, Y_batch = iterate_mini_batches(X_train, Y_train)
-                sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                step_cost = sess.run(loss,
-                                     feed_dict={x: X_batch,
-                                                y: Y_batch})
-                step_accuracy = sess.run(accuracy,
-                                         feed_dict={x: X_batch,
-                                                    y: Y_batch})
-                if i % 100 == 0:
-                    print('\tStep {i}:'.format(i))
-                    print('\t\tCost: {}'.format(step_cost))
-                    print('\t\tAccuracy: {step_accuracy}'.format(step_accuracy))
+            if epoch < epochs:
+                for batch in iterate_mini_batches(X_train, Y_train, batch_size):
+                    x_batch, y_batch, j = batch
+                    sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
+                    if j % 100 == 0:
+                        step_cost = sess.run(loss,
+                                             feed_dict={x: x_batch,
+                                                        y: y_batch})
+                        step_accuracy = sess.run(accuracy,
+                                                 feed_dict={x: x_batch,
+                                                            y: y_batch})
+                        print('\tStep {}:'.format(j))
+                        print('\t\tCost: {}'.format(step_cost))
+                        print('\t\tAccuracy: {}'.format(step_accuracy))
 
-        saved_model = saver.save(sess, save_path)
+        saved_model = loader.save(sess, save_path)
     return saved_model
-
-
-"""
-to get the mini_batch
-"""
-
-
-def iterate_mini_batches(X, Y, batch_size):
-    """
-    to get the mini_batch    
-    :param X: input data
-    :param Y: labels
-    :param batch_size: size of mini batch
-    :return: mini_batch
-    """
-    for i in range(0, X.shape[1] - batch_size + 1, batch_size):
-        X_shuffled_train, X_shuffled_valid = shuffle_data(X, Y)
-        excerpt = slice(i, i + batch_size)
-        yield X_shuffled_train[excerpt], X_shuffled_valid[excerpt]
