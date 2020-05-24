c3048fd81518b761209d97710e5e95be1816b561
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-05-24 14:30:12 -0500

    14

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -10,14 +10,14 @@ def create_batch_norm_layer(prev, n, activation):
     creates a batch normalization layer for a neural network in tensorflow
     :param prev: activated output of the previous layer
     :param n: number of nodes in the layer to be created
-    :param activation: activation function that should be used on the output of
-    the layer
+    :param activation: activation function that should be used on the output
+    of the layer
     :return:
     """
     # https://stackoverflow.com/questions/33949786/how-could-i-use-batch
     # -normalization-in-tensorflow
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    output = tf.layers.Dense(units=n, activation=activation,
+    output = tf.layers.Dense(units=n,
                              name='layer', kernel_initializer=init)
 
     Z = output(prev)
@@ -28,7 +28,10 @@ def create_batch_norm_layer(prev, n, activation):
     beta = tf.Variable(tf.constant(0.0, shape=[n]))
     gamma = tf.Variable(tf.constant(1.0, shape=[n]))
     Z_norm = tf.nn.batch_normalization(
-        Z, mean, variance, beta, gamma,
-        epsilon)
+        x=Z, mean=mean, variance=variance, offset=beta, scale=gamma,
+        variance_epsilon=epsilon)
 
-    return activation(Z_norm)
+    if not activation:
+        return Z_norm
+    else:
+        return activation(Z_norm)
