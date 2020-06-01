78c4257b1840dc5a90ca734967b3a79a441a24dc
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-01 15:09:44 -0500

    3

---
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+converts a label vector into a one-hot matrix
+"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    converts a label vector into a one-hot matrix
+    :param labels: labels
+    :param classes: last dimension of the one-hot matrix must be the number of classes
+    :return: the one-hot matrix
+    """
+    if classes is not None:
+        return K.utils.to_categorical(labels, classes)
+    else
+        return K.utils.to_categorical(labels, len(labels))
