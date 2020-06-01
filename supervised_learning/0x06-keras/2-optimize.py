8831df63d2f84dae9751412f76db04bef6647a56
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-01 14:48:08 -0500

    2

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+sets up Adam optimization for a keras model with categorical crossentropy
+loss and accuracy metrics
+"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    sets up Adam optimization for a keras model with categorical crossentropy
+    loss and accuracy metrics
+    :param network: the model to optimize
+    :param alpha: the learning rate
+    :param beta1: the first Adam optimization parameter
+    :param beta2: the second Adam optimization parameter
+    :return: None
+    """
+    network.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
+                                                beta_1=beta1,
+                                                beta_2=beta2),
+                    loss=K.losses.CategoricalCrossentropy(
+                        from_logits=True),
+                    metrics=['accuracy'])
+
+    return None
