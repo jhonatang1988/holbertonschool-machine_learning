ad13300ea4c4e467238b148bbaa31bb083eeb992
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-02 10:53:02 -0500

    2

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -19,8 +19,7 @@ def optimize_model(network, alpha, beta1, beta2):
     network.compile(optimizer=K.optimizers.Adam(lr=alpha,
                                                 beta_1=beta1,
                                                 beta_2=beta2),
-                    loss=K.losses.CategoricalCrossentropy(
-                        from_logits=True),
+                    loss='categorical_crossentropy',
                     metrics=['accuracy'])
 
     return None
