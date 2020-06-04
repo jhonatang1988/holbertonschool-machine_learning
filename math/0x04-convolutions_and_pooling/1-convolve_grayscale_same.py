302ab6802996a7afe4ffaae23bb8d80e92134600
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-04 17:58:49 -0500

    1

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,7 +5,7 @@ performs a valid convolution on grayscale images
 import numpy as np
 
 
-def convolve_grayscale_valid(images, kernel):
+def convolve_grayscale_same(images, kernel):
     """
     performs a valid convolution on grayscale images
     :param images: is a numpy.ndarray with shape (m, h, w) containing multiple
@@ -19,39 +19,20 @@ def convolve_grayscale_valid(images, kernel):
     kw is the width of the kernel
     :return: numpy.ndarray containing the convolved images
     """
-    # https://stackoverflow.com/a/43087771/4101146
-    conv_list = []
-    # for img in images:
-    #     a = img
-    #     f = kernel
-    #     s = f.shape + tuple(np.subtract(a.shape, f.shape) + 1)
-    #     strd = np.lib.stride_tricks.as_strided
-    #     subM = strd(a, shape=s, strides=a.strides * 2)
-    #     new_image = np.einsum('ij,ijkl->kl', f, subM)
-    #
-    #     conv_list.append(new_image)
-    #
-    # return np.array(conv_list)
-
-    # https://laurentperrinet.github.io/sciblog/posts/2017-09-20-the-fastest-
-    # 2d-convolution-in-the-world.html
-    # for image in images:
-    #     conv_list.append(
-    #         np.real(np.fft.ifft2(np.fft.fft2(image) * np.fft.fft2(kernel,
-    #                                                               s=image.shape))))
-    #
-    # return np.array(conv_list)
-
-    # https://d2l.ai/chapter_convolutional-neural-networks/conv-layer.html
-
     #  to pass checkers
     # """Compute 2D cross-correlation."""
     kh, kw = kernel.shape
     m, h, w = images.shape
-    Y = np.zeros((m, h - kh + 1, w - kw + 1))
+
+    pad_height = int(kernel.shape[0])
+    pad_width = int(kernel.shape[1])
+
+    padded_images = np.pad(images, ((0, 0), (pad_height, pad_height),
+                                    (pad_width, pad_width)), 'constant')
+    Y = np.empty_like(images)
     for i in range(Y.shape[1]):
         for j in range(Y.shape[2]):
-            slc = (images[:, i:i + kh, j:j + kw]) * kernel
+            slc = (padded_images[:, i:i + kh, j:j + kw]) * kernel
             # we have to get a number from result (matrix of matrices)
             slc = slc.sum(axis=1)
             slc = slc.sum(axis=1)
