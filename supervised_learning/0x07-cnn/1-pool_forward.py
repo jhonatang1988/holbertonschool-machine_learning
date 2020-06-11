658d7173355a203f35a59ac153d58dd58aef9126
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-11 12:07:38 -0500

    1

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -8,38 +8,44 @@ import numpy as np
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     performs forward propagation over a pooling layer of a neural network
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+    the output of the previous layer
+    m is the number of examples
+    h_prev is the height of the previous layer
+    w_prev is the width of the previous layer
+    c_prev is the number of channels in the previous layer
+    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for
+    the
+    pooling
+    kh is the kernel height
+    kw is the kernel width
+    stride is a tuple of (sh, sw) containing the strides for the pooling
+    sh is the stride for the height
+    sw is the stride for the width
+    mode is a string containing either max or avg, indicating whether to
+    perform
+    maximum or average pooling, respectively
+    Returns: the output of the pooling layer
     """
-    # tres paquetes para cada sample
-    imagenes = A_prev
-    num_imagenes, altura_img, largo_img, num_canales_img = imagenes.shape
-    # W se refiere al filtro
-    altura_filtro, largo_filtro = kernel_shape
-    # stride se refiere a pasos
-    altura_stride, largo_stride = stride
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
     
-    # se calcula el tamano del output y se inicializa
-    altura_output = int(
-        ((altura_img - altura_filtro) / altura_stride) + 1)
-    largo_output = int(
-        ((largo_img - largo_filtro) / largo_stride) + 1)
-    output = np.zeros(
-        (num_imagenes, altura_output, largo_output, num_canales_img))
+    # dims conv matrix
+    pool_h = int(((h_prev - kh) / sh) + 1)
+    pool_w = int(((w_prev - kw) / sw) + 1)
+    pool = np.zeros((m, pool_h, pool_w, c_prev))
     
-    # para cada stride moviendose verticalmente
-    for vertical_stride in range(altura_output):
-        # para cada stride moviendose horizontalmente
-        for horizontal_stride in range(largo_output):
-            # para cada filtro del output
-            va = vertical_stride * altura_stride
-            vaa = vertical_stride * altura_stride + altura_filtro
-            hl = horizontal_stride * largo_stride
-            hll = horizontal_stride * largo_stride + largo_filtro
-            pedacito_imagen = imagenes[:, va:vaa, hl:hll]
-            # el pooling como tal - numero_maximo_en_el_pedacito_de_imagen
-            nmeepdi = np.max(pedacito_imagen, axis=(1, 2))
-            
-            if mode == 'avg':
-                nmeepdi = np.mean(pedacito_imagen, axis=(1, 2))
-            output[:, vertical_stride, horizontal_stride] = nmeepdi
-    
-    return output
+    for i in range(pool_h):
+        for j in range(pool_w):
+            sth = i * sh
+            endh = (i * sh) + kh
+            stw = j * sw
+            endw = (j * sw) + kw
+            X = A_prev[:, sth:endh, stw:endw]
+            if mode == "max":
+                WX = np.max(X, axis=(1, 2))
+            if mode == "avg":
+                WX = np.mean(X, axis=(1, 2))
+            pool[:, i, j] = WX
+    return pool
