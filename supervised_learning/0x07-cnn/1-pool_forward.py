711b88ea3d79863201d212c3a1d6036709248a49
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-11 11:58:50 -0500

    1

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -4,30 +4,27 @@ performs forward propagation over a pooling layer of a neural network:
 """
 import numpy as np
 
-"""
-A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
-the output of the previous layer
-m is the number of examples
-h_prev is the height of the previous layer
-w_prev is the width of the previous layer
-c_prev is the number of channels in the previous layer
-kernel_shape is a tuple of (kh, kw) containing the size of the kernel for the
-pooling
-kh is the kernel height
-kw is the kernel width
-stride is a tuple of (sh, sw) containing the strides for the pooling
-sh is the stride for the height
-sw is the stride for the width
-mode is a string containing either max or avg, indicating whether to perform
-maximum or average pooling, respectively
-you may import numpy as np
-Returns: the output of the pooling layer
-"""
-
 
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     performs forward propagation over a pooling layer of a neural network
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+    the output of the previous layer
+    m is the number of examples
+    h_prev is the height of the previous layer
+    w_prev is the width of the previous layer
+    c_prev is the number of channels in the previous layer
+    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for the
+    pooling
+    kh is the kernel height
+    kw is the kernel width
+    stride is a tuple of (sh, sw) containing the strides for the pooling
+    sh is the stride for the height
+    sw is the stride for the width
+    mode is a string containing either max or avg, indicating whether to perform
+    maximum or average pooling, respectively
+    you may import numpy as np
+    Returns: the output of the pooling layer
     """
     # tres paquetes para cada sample
     imagenes = A_prev
