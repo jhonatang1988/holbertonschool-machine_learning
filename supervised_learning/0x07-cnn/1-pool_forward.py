f8c666620c5e65c07a7017fdc5acb60513e405bc
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-11 12:00:11 -0500

    1

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-performs forward propagation over a pooling layer of a neural network:
+performs forward propagation over a pooling layer of a neural network
 """
 import numpy as np
 
@@ -14,14 +14,16 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     h_prev is the height of the previous layer
     w_prev is the width of the previous layer
     c_prev is the number of channels in the previous layer
-    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for the
+    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for
+    the
     pooling
     kh is the kernel height
     kw is the kernel width
     stride is a tuple of (sh, sw) containing the strides for the pooling
     sh is the stride for the height
     sw is the stride for the width
-    mode is a string containing either max or avg, indicating whether to perform
+    mode is a string containing either max or avg, indicating whether to
+    perform
     maximum or average pooling, respectively
     you may import numpy as np
     Returns: the output of the pooling layer
