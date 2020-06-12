c17f94ef9c3d053df7ecb49ad0b99d850ae71332
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-12 13:39:22 -0500

    4

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -60,50 +60,67 @@ def lenet5(x, y):
     labels for the network
     :return:
     """
+    # el initializer 3000
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     
-    # se crea un graph con ordenes secuenciales. Se utiliza
-    # tf.models.Sequential()
-    model = tf.models.Sequential()
+    # la mierda que recibe inputs
+    primer_layer = tf.layers.Conv2D(filters=6, kernel_size=[5, 5],
+                                    padding='same', kernel_initializer=init,
+                                    activation='relu')
     
-    # ahora se crea el primer layer convolutional que tiene lo siguiente:
-    # filtros (entero) - size del kernel o filtro, que debe tener altura y
-    # largo en una tupla o lista
-    model.add(tf.layers.Conv2D(filters=6, kernel_size=[5, 5],
-                               padding='same'))
+    x = primer_layer(x)
     
     # ahora vamos con un max poolincibiribiri
-    model.add(tf.layers.MaxPooling2D(pool_size=(2, 2),
-                                     strides=(2, 2)))
+    segundo_layer = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                           strides=(2, 2))
+    
+    x = segundo_layer(x)
+    
     # ahora vamos con otra puta convolutional
-    model.add(tf.layers.Conv2D(filters=16, kernel_size=[5, 5],
-                               padding='valid'))
+    tercer_layer = tf.layers.Conv2D(filters=16, kernel_size=[5, 5],
+                                    padding='valid',
+                                    kernel_initializer=init, activation='relu')
+    
+    x = tercer_layer(x)
     
     # ahora vamos con otra piscina la mas chimba
-    model.add(tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    cuarto_layer = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                          strides=(2, 2))
+    
+    x = cuarto_layer(x)
     
     # esta mierda es para pasar las imagenes a 1D porque la vaina no
     # funciona en otras dimensions
-    model.add(tf.layers.Flatten())
+    quinto_layer = tf.layers.Flatten()
+    
+    x = quinto_layer(x)
     
     # ahora vamos con la vieja confiable de un fully connected - orgia total.
-    model.add(tf.layers.Dense(units=120, activation='relu'))
+    sexto_layer = tf.layers.Dense(units=120, activation='relu',
+                                  kernel_initializer=init)
+    
+    x = sexto_layer(x)
     
     # otra vieja confiable
-    model.add(tf.layers.Dense(units=84, activation='relu'))
+    septimo_layer = tf.layers.Dense(units=84, activation='relu',
+                                    kernel_initializer=init)
+    
+    x = septimo_layer(x)
     
     # una ultima softmax
-    softmax = model.add(tf.layers.Dense(units=10, activation='softmax'))
+    ultimo_layer = tf.layers.Dense(units=10, activation='softmax')
+    
+    x = ultimo_layer(x)
     
     # toca definir el loss para retornarlo
-    loss = tf.losses.softmax_cross_entropy()
+    loss = tf.losses.softmax_cross_entropy(y, x)
     
+    # como se optimiza la enseñanza del bb
     optimizer = tf.train.AdamOptimizer().minimize(loss)
     
-    # se compila la vuelta
-    model.compile(optimizer='adam', )
-    
     # el accuracy
-    accuracy, _ = tf.metrics.accuracy(labels=tf.argmax(y, 1),
-                                      predictions=tf.argmax(x, 1))
+    pred = tf.argmax(x, 1)
+    eq = tf.equal(pred, tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(eq, tf.float32))
     
-    return softmax, optimizer, loss, accuracy
+    return x, optimizer, loss, accuracy
