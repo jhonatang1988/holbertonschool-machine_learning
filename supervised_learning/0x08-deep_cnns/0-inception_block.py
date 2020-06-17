b81a7d122dae11d66db689177d4279b2e4a8ee9c
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-17 14:48:33 -0500

    0

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -28,65 +28,52 @@ def inception_block(A_prev, filters):
     
     # esta convolution va directo entre inputs y outputs
     non_linear_1_1_A = K.layers.Conv2D(
-        filters=F1,
-        kernel_size=[1, 1],
-        kernel_initializer=init,
-        activation='relu',
-        padding='same'
+        filters=F1, kernel_size=[1, 1],
+        kernel_initializer=init, activation='relu', padding='same'
     )
     to_concatenate_A = non_linear_1_1_A(A_prev)
     
     # esta convolution coge inputs pero va a otra convolution de 3x3
     non_linear_1_1_B = K.layers.Conv2D(
-        filters=F3R,
-        kernel_size=[1, 1],
-        activation='relu'
+        filters=F3R, kernel_size=[1, 1],
+        activation='relu', padding='same'
     )
     
     to_conv_A = non_linear_1_1_B(A_prev)
     
     # esta convolution coge inputs pero va a otra convolution de 5x5
     non_linear_1_1_C = K.layers.Conv2D(
-        filters=F5R,
-        kernel_size=[1, 1],
-        activation='relu'
+        filters=F5R, kernel_size=[1, 1],
+        activation='relu', padding='same'
     )
     
     to_conv_B = non_linear_1_1_C(A_prev)
     
     # esta max pooling coge inputs pero va a una convolution de 1x1
     linear_pool = K.layers.MaxPooling2D(
-        pool_size=(3, 3),
-        strides=(1, 1),
-        padding='same'
+        pool_size=(3, 3), strides=(1, 1), padding='same'
     )
     
     to_conv_C = linear_pool(A_prev)
     
     # esta convolution coge el input de la 1x1 convolution
     non_linear_1_1_E = K.layers.Conv2D(
-        filters=F3,
-        kernel_size=[3, 3],
-        activation='relu',
-        padding='same'
+        filters=F3, kernel_size=[3, 3],
+        activation='relu', padding='same',
     )
     
     to_concatenate_B = non_linear_1_1_E(to_conv_A)
     
     non_linear_1_1_F = K.layers.Conv2D(
-        filters=F5,
-        kernel_size=[5, 5],
-        activation='relu',
-        padding='same'
+        filters=F5, kernel_size=[5, 5],
+        activation='relu', padding='same'
     )
     
     to_concatenate_C = non_linear_1_1_F(to_conv_B)
     
     non_linear_1_1_G = K.layers.Conv2D(
-        filters=FPP,
-        kernel_size=[1, 1],
-        activation='relu',
-        padding='same'
+        filters=FPP, kernel_size=[1, 1],
+        activation='relu', padding='same'
     )
     
     to_concatenate_D = non_linear_1_1_G(to_conv_C)
