af6acb541a1ec94e5299b73ca6f4814d807d9271
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-06-17 15:25:10 -0500

    0

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -15,7 +15,7 @@ def lenet5(X):
     :return: a K.Model compiled to use Adam optimization (with default
     hyperparameters) and accuracy metrics
     """
-    
+
     init = K.initializers.he_normal()
     primer_layer = K.layers.Conv2D(
         filters=6,
@@ -24,53 +24,53 @@ def lenet5(X):
         activation='relu',
         padding='same'
     )
-    
+
     Y_pred = primer_layer(X)
-    
+
     segundo_layer = K.layers.MaxPooling2D(pool_size=(2, 2),
                                           strides=(2, 2))
-    
+
     Y_pred = segundo_layer(Y_pred)
-    
+
     tercer_layer = K.layers.Conv2D(filters=16,
                                    kernel_size=[5, 5],
                                    kernel_initializer=init,
                                    activation='relu'
                                    )
-    
+
     Y_pred = tercer_layer(Y_pred)
-    
+
     cuarto_layer = K.layers.MaxPooling2D(pool_size=(2, 2),
                                          strides=(2, 2))
-    
+
     Y_pred = cuarto_layer(Y_pred)
-    
+
     quinto_layer = K.layers.Flatten()
-    
+
     Y_pred = quinto_layer(Y_pred)
-    
+
     sexto_layer = K.layers.Dense(units=120,
                                  kernel_initializer=init,
                                  activation='relu')
-    
+
     Y_pred = sexto_layer(Y_pred)
-    
+
     septimo_layer = K.layers.Dense(units=84,
                                    kernel_initializer=init,
                                    activation='relu')
-    
+
     Y_pred = septimo_layer(Y_pred)
-    
+
     ultimo_layer_con_activacion = K.layers.Dense(units=10,
                                                  kernel_initializer=init,
                                                  activation='softmax')
-    
+
     Y_pred = ultimo_layer_con_activacion(Y_pred)
-    
+
     model = K.models.Model(inputs=X, outputs=Y_pred)
     
     model.compile(loss='categorical_crossentropy',
                   optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
-    
+
     return model
